2c4a713c31af860fadcafd1567124600141ad411
DougKittelsen <dkittelsen@gmail.com>
2017-02-09 16:29:19 -0700

    Updated Example with credentials
    Service credentials updated (user/pass/workspace)

--- examples/conversation_v1.py
+++ examples/conversation_v1.py
@@ -2,15 +2,15 @@ import json
 from watson_developer_cloud import ConversationV1
 
 conversation = ConversationV1(
-    username='YOUR SERVICE USERNAME',
-    password='YOUR SERVICE PASSWORD',
+    username='d0e3189a-8084-4d74-adab-aedb11ac09ea',
+    password='O0bjw1EztPqb',
     version='2016-09-20')
 
 # replace with your own workspace_id
-workspace_id = 'b42ee794-c019-4a0d-acd2-9e4d1d016767'
+workspace_id = 'd148117e-a0a7-44f2-95b0-d92cc73d6818'
 
 response = conversation.message(workspace_id=workspace_id, message_input={
-    'text': 'What\'s the weather like?'})
+    'text': 'I think I have diabetes'})
 print(json.dumps(response, indent=2))
 
 # When you send multiple requests for the same conversation, include the
@@ -19,3 +19,10 @@ print(json.dumps(response, indent=2))
 # 'text': 'turn the wipers on'},
 #                                context=response['context'])
 # print(json.dumps(response, indent=2))
+
+
+{
+  "url": "https://gateway.watsonplatform.net/conversation/api",
+  "username": "d0e3189a-8084-4d74-adab-aedb11ac09ea",
+  "password": "O0bjw1EztPqb"
+}
